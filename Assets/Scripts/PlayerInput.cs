aaa99b4f937cc3d3d520d2ac58bbcdd5c89a03e5
Jeffreeyy <17844@ma-web.nl>
2016-05-24 12:19:12 +0200

    Added player scripts

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerInput : MonoBehaviour 
+{
+    private PlayerMovement _movement;
+
+    void Awake()
+    {
+        _movement = GetComponent<PlayerMovement>();
+    }
+
+	void FixedUpdate () 
+    {
+        //UP
+	    if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+        }
+        //DOWN
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+        }
+        //LEFT
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _movement.MoveLeft();
+        }
+        //RIGHT
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _movement.MoveRight();
+        }
+        //ABILITY 1
+        if (Input.GetKey(KeyCode.Z))
+        {
+
+        }
+        //ABILITY 2
+        if (Input.GetKey(KeyCode.X))
+        {
+
+        }
+
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    [SerializeField]private float _movementSpeed;
+
+    public void JumpUp()
+    {
+        
+    }
+
+    public void DuckDown()
+    {
+
+    }
+
+    public void MoveLeft()
+    {
+        transform.Translate((Vector2.left) * _movementSpeed * Time.fixedDeltaTime);
+    }
+
+    public void MoveRight()
+    {
+        transform.Translate((Vector2.right) * _movementSpeed * Time.fixedDeltaTime);
+    }
+
+
+}
