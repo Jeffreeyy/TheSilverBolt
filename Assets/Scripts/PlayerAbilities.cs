2f0af560daef41d237f552c26591f126230f2d26
Lorenzo <18927@ma-web.nl>
2016-05-25 10:04:52 +0200

    particles now emit when speed boef = enabled

--- Assets/Scripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerAbilities.cs
@@ -7,20 +7,26 @@ public class PlayerAbilities : MonoBehaviour {
     private PlayerMovement _movement;
     [SerializeField]private float _normalMoveSpeed;
     [SerializeField]private float _enhancedMoveSpeed;
+    private ParticleSystem _particleSystem;
 
     void Start()
     {
+        _particleSystem = GetComponentInChildren<ParticleSystem>();
         _movement = GetComponent<PlayerMovement>();
         _normalMoveSpeed = _movement.MovementSpeed;
     }
 
     public void ActivateSpeedBuff()
     {
+        var em = _particleSystem.emission;
         _movement.MovementSpeed = _enhancedMoveSpeed;
+        em.enabled = true;
     }
 
     public void DeactivateSpeedBuff()
     {
+        var em = _particleSystem.emission;
         _movement.MovementSpeed = _normalMoveSpeed;
+        em.enabled = false;
     }
 }
