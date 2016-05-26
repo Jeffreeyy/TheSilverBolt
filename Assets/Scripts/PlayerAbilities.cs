3344a6ed0b0b62312650de1e5a44830c2be5c446
Lorenzo <18927@ma-web.nl>
2016-05-26 08:45:00 +0200

    changes is lorenzo scene

--- Assets/Scripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerAbilities.cs
@@ -7,26 +7,33 @@ public class PlayerAbilities : MonoBehaviour {
     private PlayerMovement _movement;
     [SerializeField]private float _normalMoveSpeed;
     [SerializeField]private float _enhancedMoveSpeed;
-    private ParticleSystem _particleSystem;
+    private ParticleSystem[] _particleSystem;
 
     void Start()
     {
-        _particleSystem = GetComponentInChildren<ParticleSystem>();
+        _particleSystem = GetComponentsInChildren<ParticleSystem>();
         _movement = GetComponent<PlayerMovement>();
         _normalMoveSpeed = _movement.MovementSpeed;
     }
 
+    private void EmitParticles(bool activate)
+    {
+        for (int i = 0; i < _particleSystem.Length; i++)
+        {
+            var em = _particleSystem[i].emission;
+            em.enabled = activate;
+        }
+    }
+
     public void ActivateSpeedBuff()
     {
-        var em = _particleSystem.emission;
+        EmitParticles(true);
         _movement.MovementSpeed = _enhancedMoveSpeed;
-        em.enabled = true;
     }
 
     public void DeactivateSpeedBuff()
     {
-        var em = _particleSystem.emission;
+        EmitParticles(false);
         _movement.MovementSpeed = _normalMoveSpeed;
-        em.enabled = false;
     }
 }
