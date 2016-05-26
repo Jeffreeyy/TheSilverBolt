7db8965c9b353257402fbe9392cc83993fbf5dd1
Jeffreeyy <17844@ma-web.nl>
2016-05-26 08:55:51 +0200

    Added camera

---
+++ Assets/CameraTargetFollower.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTargetFollower : MonoBehaviour 
+{
+    [SerializeField]private Transform _target;
+    [SerializeField]private float _damping;
+    [SerializeField]private Vector3 _offset;
+    private float _newYPos;
+    private Vector3 _newPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        _newPos = new Vector3(_target.position.x - _offset.x, _newYPos, -10f);
+        transform.position = Vector3.Lerp(transform.position, _newPos, _damping * Time.deltaTime);
+
+        if(_target.position.y > 0f)
+        {
+            _newYPos = _target.position.y;
+        }
+        else
+        {
+            _newYPos = 0f;
+        }
+	}
+}
