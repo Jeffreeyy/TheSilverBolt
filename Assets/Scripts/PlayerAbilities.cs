5c9e3d6f726ef6be19d0426e3209116659a932ee
bennie12345 <bmuijldert@gmail.com>
2016-05-26 11:30:21 +0200

    added power slider code+palceholders

--- Assets/Scripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerAbilities.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class PlayerAbilities : MonoBehaviour {
 
     private PlayerMovement _movement;
+    private PlayerPower _playerPower;
     [SerializeField]private float _normalMoveSpeed;
     [SerializeField]private float _enhancedMoveSpeed;
     private ParticleSystem[] _particleSystem;
@@ -13,6 +14,7 @@ public class PlayerAbilities : MonoBehaviour {
     {
         _particleSystem = GetComponentsInChildren<ParticleSystem>();
         _movement = GetComponent<PlayerMovement>();
+        _playerPower = GetComponent<PlayerPower>();
         _normalMoveSpeed = _movement.MovementSpeed;
     }
 
@@ -27,13 +29,22 @@ public class PlayerAbilities : MonoBehaviour {
 
     public void ActivateSpeedBuff()
     {
-        EmitParticles(true);
-        _movement.MovementSpeed = _enhancedMoveSpeed;
+        if (_playerPower.Power > 0 && !_playerPower.IsUsingPower)
+        {
+            _playerPower.IsUsingPower = true;
+            EmitParticles(true);
+            _movement.MovementSpeed = _enhancedMoveSpeed;
+            StartCoroutine(_playerPower.PowerDrain());
+            
+        }
     }
 
     public void DeactivateSpeedBuff()
     {
+        _playerPower.IsUsingPower = false;
         EmitParticles(false);
         _movement.MovementSpeed = _normalMoveSpeed;
     }
+
+    
 }
---
+++ Assets/Scripts/PlayerPower.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPower : MonoBehaviour 
+{
+    private float _drainRate = .1f;
+    private float _drainAmount = 1;
+    private float _rechargeRate = .1f;
+    private float _rechargeAmount = .2f;
+    private float _maxPower;
+    private float _power = 100;
+    public float Power
+    {
+        get
+        {
+            return _power;
+        }
+        set
+        {
+            _power = value;
+        }
+    }
+
+    private bool _isUsingPower = false;
+    public bool IsUsingPower
+    {
+        get
+        {
+            return _isUsingPower;
+        }
+        set
+        {
+            _isUsingPower = value;
+        }
+    }
+    private PlayerAbilities _playerAbilities;
+
+    private bool _isRecharging = false;
+
+    // use this for initialization
+    void Start()
+    {
+        _playerAbilities = GetComponent<PlayerAbilities>();
+        _maxPower = _power;
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        Recharge();
+    }
+
+    private void Recharge()
+    {
+        if (!_isUsingPower && _power < _maxPower && !_isRecharging)
+        {
+            StartCoroutine(Recharging());
+        }
+        else if (_isUsingPower)
+        {
+            _isRecharging = false;
+        }
+    }
+
+    public IEnumerator PowerDrain()
+    {
+        while (_isUsingPower)
+        {
+            yield return new WaitForSeconds(_drainRate);
+            _power -= _drainAmount;
+        }
+    }
+
+    IEnumerator Recharging()
+    {
+        _isRecharging = true;
+        while (!_isUsingPower)
+        {
+            yield return new WaitForSeconds(_rechargeRate);
+            _power += _rechargeAmount;
+        }
+    }
+}
